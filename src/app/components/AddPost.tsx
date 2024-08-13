8120f21a3ad0f9c5a42a4c99d777c48427e2b29d
Nikhilka4 <106381060+Nikhilka4@users.noreply.github.com>
2024-08-13 20:09:30 +0530

    homepage under processing

--- src/app/components/AddPost.tsx
+++ src/app/components/AddPost.tsx
@@ -1,6 +1,42 @@
+import Image from "next/image";
+
 const AddPost = () => {
   return (
-    <div>AddPost</div>
-  )
-}
-export default AddPost
+    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+      {/* avatar */}
+      <Image
+        src="https://images.pexels.com/photos/18028832/pexels-photo-18028832/free-photo-of-road-through-forest-in-summer.png?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        alt=""
+        width={48}
+        height={48}
+        className="w-12 h-12 object-contain rounded-full"
+      />
+      {/* post */}
+      <div className="flex-1">
+        {/* text input */}
+        <div className="flex gap-4">
+          <textarea placeholder="What's on your mind/" className="flex-1 p-2 bg-slate-100 rounded-lg"></textarea>
+          <Image
+            src="/emoji.png"
+            alt=""
+            width={20}
+            height={20}
+            className="w-5 h-5 cursor-pointer self-end"
+          />
+        </div>
+        {/* post options */}
+        <div className="flex items-center gap-4 mt-4 text-gray-400">
+          <div className="">
+          <Image
+            src="/addimage.png"
+            alt=""
+            width={20}
+            height={20}
+          />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AddPost;
--- src/app/components/Stories.tsx
+++ src/app/components/Stories.tsx
@@ -2,11 +2,177 @@ import Image from "next/image";
 
 const Stories = () => {
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md overflow-auto text-sm">
-      <div className="flex gap-8">
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-auto text-sm scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        {/* story */}
         <div className="flex flex-col items-center gap-2 cursor-pointer">
           <Image
-            src=""
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            className="w-20 h-20 rounded-full ring-2"
+            width={80}
+            height={80}
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/videos/15720123/pexels-photo-15720123.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
             alt=""
             className="w-20 h-20 rounded-full ring-2"
             width={80}
